870f60b4af141c55c044dab0e36b33d8219f7e10
Fadodo <fadydrive96@gmail.com>
2018-12-18 09:02:52 -0800

    added more articles

--- client/src/components/CommentCard/commentcard.js
+++ client/src/components/CommentCard/commentcard.js
@@ -8,12 +8,11 @@ export function CommentCard(props) {
     return (
         <div className = {props.className}>
            
+                            <i className="threadline"></i>
                             <h5 className="card-title">{props.name}</h5>
                             <p className="card-text">{props.comment}</p>
                             <Button className = {props.className1} onClick={props.toggle} value={props.value} color="primary">Reply</Button>
                             <hr></hr>
-              
-
             </div>
 
                 
